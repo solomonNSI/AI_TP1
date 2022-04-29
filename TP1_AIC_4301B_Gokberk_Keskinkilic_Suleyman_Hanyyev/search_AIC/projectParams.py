5214deb6288b40d4bd3ec4c4c08075a441fc4299
Gökberk Keskinkılıç <gkeskinkilic@gmail.com>
2022-04-29 13:05:39 +0200

    tp1 klasör

---
+++ TP1_AIC_4301B_Gokberk_Keskinkilic_Suleyman_Hanyyev/search_AIC/projectParams.py
@@ -0,0 +1,14 @@
+# projectParams.py
+# ----------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+
+
+
+STUDENT_CODE_DEFAULT = 'searchAgents.py,search.py'
+PROJECT_TEST_CLASSES = 'searchTestClasses.py'
+PROJECT_NAME = 'Project 1: Search'
+BONUS_PIC = False
